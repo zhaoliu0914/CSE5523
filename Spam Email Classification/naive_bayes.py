39e9cac057482c579f18c084830855a4f34fbeb4
Zhao Liu <liu.9553@osu.edu>
2024-02-15 22:08:32 -0500

    add comments and explain how this naive bayes works

--- Spam Email Classification/naive_bayes.py
+++ Spam Email Classification/naive_bayes.py
@@ -1,11 +1,40 @@
+import datetime
 import numpy as np
 import pandas as pd
 
 SPAM = 1
 NON_SPAM = 0
 
+"""
+For every new test data, we wish to calculate whether it's a spam(Y=1) or it's a non-spam(Y=0),
+so the formular is P(Y|X) = [P(X|Y)P(Y)]/P(X)
+
+Since we want to predict whether Y=1 or Y=0,
+we only need to compare P(Y=1|X) = [P(X|Y=1)P(Y=1)]/P(X) and P(Y=0|X) = [P(X|Y=0)P(Y=0)]/P(X)
+
+Since [P(X|Y=1)P(Y=1)]/P(X) and [P(X|Y=0)P(Y=0)]/P(X) have same P(X) and it does not affect the final answer,
+we could simply eliminate the P(X) from both equations.
+
+For the Naive Bayes, we treat every X in P(X|Y) is conditional independent to each other,
+we could module P(X|Y) = Sigma from 1 to d dimension P(X[d]|Y).
+For example, P(X|Y) = P(X[1]|Y) * P(X[2]|Y) * P(X[3]|Y) * .......... * P(X[57]|Y)
+As the result, P(Y=1|X) = P(X[1]|Y=1) * P(X[2]|Y=1) * P(X[3]|Y=1) * .......... * P(X[57]|Y=1) * P(Y=1)
+               P(Y=0|X) = P(X[1]|Y=0) * P(X[2]|Y=0) * P(X[3]|Y=0) * .......... * P(X[57]|Y=0) * P(Y=0)
+
+We just simply split each column data to 2 groups,
+one group is all the values of each column less than and equal to median
+another group is all the values of each column greater than median
+
+Since each column only has 2 status less equal and greater than median, we could apply Bernoulli Distribution to columns.
+For example, P(X[1]|Y=1) will become P(X[1] <= median|Y=1) or P(X[1] > median|Y=1)
+
+If P(Y=1|X) is greater than P(Y=1|X), we could decide this data/row is a spam(Y=1), and vice versa
+"""
 
 if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+    print(f"Start Time: {start_time}")
+
     train = pd.read_csv("spambase.train", header=None)
     test = pd.read_csv("spambase.test", header=None)
 
@@ -37,37 +66,59 @@ if __name__ == '__main__':
     # Probability(Y = 0)
     probability_y_0 = number_train_y_0 / train_rows
 
+    # Calculate the median of each column, and store them into medians[]
     medians = []
     for column in range(train_columns - 1):
         medians.append(train[column].median())
         # print(f"column = {column}, median = {train[column].median()}")
 
-    """
-    median_condition_probability = np.array([[1, 2, 3]])
 
+    # We just simply split each column data to 2 groups,
+    # one group is all the values of each column less than and equal to median
+    # another group is all the values of each column greater than median
+    # Since each column only has 2 status less equal and greater than median, we could apply Bernoulli Distribution to columns.
+    # For example, P(X[1]|Y=1) will become P(X[1] <= median|Y=1) or P(X[1] > median|Y=1)
+    # Based on the value of median,
+    # calculate the posterior probability of P(X[d]<=median|Y=1) and P(X[d]<=median|Y=0) for each column
+    # where X[d]<=median is strict less than and equal to median "<=", and X[d], d indicates each column.
+    # For P(X[d]>median|Y=1) = 1 - P(X[d]<=median|Y=1)
+    # store all the posterior to condition_probability,
+    # condition_probability[0] = theta_0 while condition_probability[1] = theta_1
+    condition_probability = np.array([[0, 0]])
     for column in range(train_columns - 1):
         median = medians[column]
 
+        # Calculate the posterior probability of P(X[d]<=median|Y=1) and call it theta_1
         number_match_1 = train_y_1[train_y_1[column] <= median].shape[0]
         theta_1 = number_match_1 / number_train_y_1
 
+        # Calculate the posterior probability of P(X[d]<=median|Y=0) and call it theta_0
         number_match_0 = train_y_0[train_y_0[column] <= median].shape[0]
         theta_0 = number_match_0 / number_train_y_0
 
-        row_median_proba = np.array([[median, theta_1, theta_0]])
+        # condition_probability[0] = theta_0 while condition_probability[1] = theta_1
+        column_condition_proba = np.array([[theta_0, theta_1]])
 
+        # Concatenate all posterior conditional probability of each column, and there are total 57 columns,
+        # so condition_probability has 57 dimensions/rows
         if column == 0:
-            median_condition_probability = row_median_proba
+            condition_probability = column_condition_proba
         else:
+            condition_probability = np.concatenate((condition_probability, column_condition_proba))
 
-            median_condition_probability = np.concatenate((median_condition_probability, row_median_proba))
-    """
-    #print(median_condition_probability)
-    #number_match_1 = train_y_1[train_y_1[11] < 0.14500000000000002].shape[0]
-    #print(f"number_match_1 = {number_match_1}")
-    #print(f"number_train_y_1 = {number_train_y_1}")
-    #print(f"number_train_y_1 = {number_train_y_0}")
-
+    # Predict process
+    # For every new test data, we wish to calculate whether it's a spam(Y=1) or it's a non-spam(Y=0)
+    # So the formular is P(Y|X) = [P(X|Y)P(Y)]/P(X)
+    # Since we want to predict whether Y=1 or Y=0,
+    # we only need to compare P(Y=1|X) = [P(X|Y=1)P(Y=1)]/P(X) and P(Y=0|X) = [P(X|Y=0)P(Y=0)]/P(X)
+    # Since [P(X|Y=1)P(Y=1)]/P(X) and [P(X|Y=0)P(Y=0)]/P(X) have same P(X) and it does not affect the final answer,
+    # we could simply eliminate the P(X) from both equations.
+    # For the Naive Bayes, we treat every X in P(X|Y) is conditional independent to each other,
+    # we could module P(X|Y) = Sigma from 1 to d dimension P(X[d]|Y).
+    # For example, P(X|Y) = P(X[1]|Y) * P(X[2]|Y) * P(X[3]|Y) * .......... * P(X[57]|Y)
+    # As the result, P(Y=1|X) = P(X[1]|Y=1) * P(X[2]|Y=1) * P(X[3]|Y=1) * .......... * P(X[57]|Y=1) * P(Y=1)
+    #                P(Y=0|X) = P(X[1]|Y=0) * P(X[2]|Y=0) * P(X[3]|Y=0) * .......... * P(X[57]|Y=0) * P(Y=0)
+    # If P(Y=1|X) is greater than P(Y=1|X), we could decide this data/row is a spam(Y=1), and vice versa
     for row in range(test_rows):
         predict_value = 0
         real_value = test.iloc[row, test_columns - 1]
@@ -78,42 +129,57 @@ if __name__ == '__main__':
         probability_non_spam = 1
 
         for column in range(test_columns - 1):
+
             new_value = test.iloc[row, column]
 
             median = medians[column]
 
+            """
             number_match_1 = train_y_1[train_y_1[column] <= median].shape[0]
             theta_1 = number_match_1 / number_train_y_1
 
             number_match_0 = train_y_0[train_y_0[column] <= median].shape[0]
             theta_0 = number_match_0 / number_train_y_0
+            """
 
+            # get posterior conditional probability for each column
+            # P(X[d]|Y=1), like P(X[1]|Y=1)
+            theta_0 = condition_probability[column][0]
+            theta_1 = condition_probability[column][1]
+
+            # For P(X[d]>median|Y=1) = 1 - P(X[d]<=median|Y=1)
+            # For P(X[d]>median|Y=0) = 1 - P(X[d]<=median|Y=0)
             if new_value > median:
                 theta_1 = 1 - theta_1
                 theta_0 = 1 - theta_0
 
+            # Multiply all posterior conditional probabilities together
             probability_spam = probability_spam * theta_1
             probability_non_spam = probability_non_spam * theta_0
 
+        # Multiply all posterior conditional probabilities and P(Y=1)
         probability_spam = probability_spam * probability_y_1
         probability_non_spam = probability_non_spam * probability_y_0
 
-        #print(f"row = {row}, probability_spam = {probability_spam}, probability_non_spam = {probability_non_spam}")
-
+        # Determine whether current data is spam(1) or non-spam(0), based on the probability of P(Y=1|X) and P(Y=0|X)
         if probability_spam >= probability_non_spam:
             predict_value = 1
         else:
             predict_value = 0
-
-        #print(f"row = {row}, real_value = {real_value}, predict_value = {predict_value}")
 
         if predict_value == real_value:
             number_correct = number_correct + 1
         else:
             number_wrong = number_wrong + 1
 
+    end_time = datetime.datetime.now()
+    print(f"End Time: {end_time}")
+    print(f"Total spend time: {end_time - start_time}")
+
     print(f"number_correct = {number_correct}")
     print(f"number_wrong = {number_wrong}")
 
     print(f"Test Error: {number_wrong / test_rows}")
+
+
 
--- Spam Email Classification/numpy_example.py
+++ Spam Email Classification/numpy_example.py
@@ -1,4 +1,9 @@
+import datetime
 import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(f"arr[0][1] = {arr[0][1]}")
 
 a = np.array([[1, 2, 3]])
 
@@ -10,3 +15,5 @@ b = np.concatenate((a, [[4, 5, 6]]))
 b = np.concatenate((b, [[7, 8, 9]]))
 
 print(b)
+
+print(datetime.datetime.now())
